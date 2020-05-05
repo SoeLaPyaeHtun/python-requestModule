4e5d95cc93696cb070e62eef1c0901b9ef84e89d
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2020-05-05 17:22:19 +0630

    bs4

--- 1.py
+++ 1.py
@@ -1,12 +1,20 @@
 import requests
 import re
+from bs4 import BeautifulSoup
 
-result = requests.get("http://creatigon.com/")
+result = requests.get("https://roadtoburmatravel.com/")
 
 
 test = result.text
-searchresult = re.findall("<img src=\"(.*)\" alt=\"(.*)\">", test)
+soup = BeautifulSoup(test, 'html.parser')
+for i in soup.find_all('a'):
+    print(i.get('href'))
+    
+#searchresult = re.findall("<img src=\"(.*)\" alt=\"(.*)\">", test)
+#searchlink = re.findall("src=\'(.*)\'", test)
 
-for i in searchresult:
-    print(i[0])
+#for i in searchresult:
+ #   print(i[0])
 
+# for i  in searchlink:
+#     print(i)
